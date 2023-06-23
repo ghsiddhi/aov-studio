5958f8951bbf9cad6f93fc0a4f6baa4607379cf1
siddhi-gh <siddhiusgupta@gmail.com>
2023-06-23 22:43:52 +0530

    FIX: fix imports in App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Header } from "./header/Header";
-import { Hero } from "./hero/Hero";
-import { About } from "./about/About";
-import { Services } from "./services/Services";
-import { Portfolio } from "./portfolio/Portfolio";
-import { Blog } from "./blog/Blog";
-import { Footer } from "./footer/Footer";
+import { Header } from "./sections/header/Header";
+import { Hero } from "./sections/hero/Hero";
+import { About } from "./sections/about/About";
+import { Services } from "./sections/services/Services";
+import { Portfolio } from "./sections/portfolio/Portfolio";
+import { Blog } from "./sections/blog/Blog";
+import { Footer } from "./sections/footer/Footer";
 
 const App = () => {
   return (
