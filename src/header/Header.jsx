5775b3490e4e785b1d5fd344053875c9fa9a51e1
siddhi-gh <siddhiusgupta@gmail.com>
2023-06-23 21:41:49 +0530

    FEAT: integrate hamburger menu logic

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,34 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "./headerStyles.css";
 
 export const Header = () => {
+  const [navActive, setNavActive] = useState(false);
+
+  const toggleResponsiveNav = () => {
+    setNavActive(!navActive);
+  };
+
+  const handleNavLinkClick = () => {
+    setNavActive(false);
+  };
+
   return (
-    <header className="contained">
+    <header className={`contained ${navActive ? "active" : ""}`}>
       <h1 className="visually-hidden">
         An architecture and interior design firm
       </h1>
-      <a href="#" className="logo">
-        Architect
+      <a href="/" className="logo">
+        AOV Studio
       </a>
 
-      <span className="menu-icon hamburger-menu">&#9776;</span>
-      <nav>
-        <span className="menu-icon close-menu">&#215;</span>
-        <ul>
+      <span
+        className={`menu-icon hamburger-menu ${navActive ? "active" : ""}`}
+        onClick={toggleResponsiveNav}
+      >
+        &#9776;
+      </span>
+      <nav className={`nav ${navActive ? "active" : ""}`}>
+        <span
+          className={`menu-icon close-menu ${
+            navActive ? "contained active" : ""
+          }`}
+          onClick={toggleResponsiveNav}
+        >
+          &#215;
+        </span>
+        <ul className={`nav-list ${navActive ? "contained" : ""}`}>
           <li>
-            <a href="#">Home</a>
+            <a href="/" onClick={handleNavLinkClick}>
+              Home
+            </a>
           </li>
           <li>
-            <a href="#about">About</a>
+            <a href="#about" onClick={handleNavLinkClick}>
+              About
+            </a>
           </li>
           <li>
-            <a href="#services">Services</a>
+            <a href="#services" onClick={handleNavLinkClick}>
+              Services
+            </a>
           </li>
           <li>
-            <a href="#portfolio">Portfolio</a>
+            <a href="#portfolio" onClick={handleNavLinkClick}>
+              Portfolio
+            </a>
           </li>
           <li>
-            <a href="#footer">Contact</a>
+            <a href="#footer" onClick={handleNavLinkClick}>
+              Contact
+            </a>
           </li>
         </ul>
       </nav>
